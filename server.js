1e91164c27ea5893a0b89f39617d8361f70d291c
Atharv Soni <atharvsoni@Atharvs-MacBook-Pro-2.local>
2025-10-30 17:53:32 +0530

    login route added with error handling

--- server.js
+++ server.js
@@ -32,3 +32,35 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("we need everything for details");
   }
 });
+
+app.post('/login', async(req, res) => {
+    let username = req.body.username
+    let email = req.body.email
+    let password = req.body.password 
+    if ((username && password) || (email && password)){
+        let user = await prisma.user.findFirst({
+            where : {
+                OR : [
+                    {username : username},
+                    {email : email}
+                ]
+            }
+        })
+
+        if (!user){
+            res.status(400).json({message : "Signup First!"})
+        }
+        else{
+            let validate = await bcrypt.compare(password, user.password)
+            if (validate){
+                let token = jwt.sign({username : user.username, role : user.role}, "Rps@220068", {expiresIn : "5h"})
+                res.status(200).json({"token" : token})
+            }
+            else{
+                res.status(400).json({message : "Invalid Password"})
+            }
+        }
+    }else {
+        res.status(400).json({message : "All Fields Required!"})
+    }
+})
