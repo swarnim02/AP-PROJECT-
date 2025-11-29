efec1b8fc132276e9f4e3abb0d33eb789460e585
ish1416 <ish1442006@gmail.com>
2025-11-30 00:25:55 +0530

    "Added jwt token singnature"

---
+++ auth-module/tokenUtils.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+exports.generateToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      name: user.name,
+      role: user.role,
+      year: user.year
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "8h" }
+  );
+};
+
+
