c68d2071b2a8a39e52047124e336a981de640136
Ansh <bahetianshh@gmail.com>
2025-12-01 09:08:50 +0530

    added route for update profile

--- backend/src/modules/auth/routes/authRoutes.js
+++ backend/src/modules/auth/routes/authRoutes.js
@@ -4,5 +4,6 @@ const { signup, login } = require("../controllers/authController");
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.put('/profile', auth, updateProfile);
 
 module.exports = router;
